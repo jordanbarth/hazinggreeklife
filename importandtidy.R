31a78a0917113679447fd777a0a60fac3ca960ad
YOUR FULL NAME <jb5622a@student.american.edu>
2018-04-29 19:57:03 -0400

    4.28 Update
    8pm

--- importandtidy.R
+++ importandtidy.R
@@ -3,6 +3,11 @@ library(dplyr)
 library(readxl)
 library(ggplot2)
 library(lubridate)
+library(maps)
+library(ggmap)
+library(mapdata)
+
+# Import and Tiday
 
 hazeglife <- read_excel("~/Desktop/hazinggreeklife/Hazing Database 4.25 presentation.xlsx")
 hazeglife
@@ -20,30 +25,11 @@ sanction <- hazeglife$'Resulting Action'
 enddate <- hazeglife$'End Date'
 camplat <- hazeglife$'CampusLat (N/S)'
 camplon <- hazeglife$'CampLong (E/W)'
-                                                                          
+campsize <- hazeglife$'Campus Size'
 hazeglife %>% 
   mutate('Suspension Length' = difftime(startdate, enddate))
 
-unique(minwage$chain)
-unique(hazeglife$`Resulting Action`)
-filter(hazeglife, Organization == "Sigma Alpha Epsilon")
-filter(minwage, chain == "burgerking") 
-# filter(minwage, chain == "kfc") 
-# filter(minwage, chain == "roys")
-# unique(minwage$location)
-# filter(minwage, location == "PA")
-# # filter(minwage, location == "centralNJ")
-# # filter(minwage, location == "northNJ")
-# # filter(minwage, location == "shoreNJ")
-# # filter(minwage, location == "southNJ")
-# tb2_cases <- filter(table2, type == "cases")$'count'
-# tb2_country <- filter(table2, type == "cases")$'country'
-# tb2_year <- filter(table2, type == "cases")$'year'
-# tb2_population <- filter(table2, type == "population")$'count'
-# table2_clean <- tibble(country = tb2_country,
-# year = tb2_year,
-# rate = tb2_cases / tb2_population)
-# table2_clean
+# Graphing
 
 orgsyr <- tribble(
   ~year, ~noforgs,
@@ -86,3 +72,40 @@ orgscamp <- tribble(
 
 ggplot(data = orgscamp) +
   geom_bar(mapping = aes(x = institution, y = norgs), stat = "identity")
+
+# mapping
+
+usa <- map_data("usa") # we already did this, but we can do it again
+ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
+  coord_fixed(1.3)
+
+labs <- data.frame(
+  long = c(-122.064873, -122.306417),
+  lat = c(36.951968, 47.644855),
+  names = c("SWFSC-FED", "NWFSC"),
+  stringsAsFactors = FALSE
+)  
+
+gg1 + 
+  geom_point(data = labs, aes(x = long, y = lat), color = "black", size = 5) +
+  geom_point(data = labs, aes(x = long, y = lat), color = "yellow", size = 4)
+
+#subset of states 
+west_coast <- subset(states, region %in% c("california", "oregon", "washington"))
+
+ggplot(data = west_coast) + 
+  geom_polygon(aes(x = long, y = lat, group = group), fill = "palegreen", color = "black") + 
+  coord_fixed(1.3)
+
+#Plot the state first but let’s ditch the axes gridlines, and gray background by using the super-wonderful theme_nothing().
+
+ca_base <- ggplot(data = ca_df, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "gray")
+ca_base + theme_nothing()
+
+#Now plot the county boundaries in white:
+  
+  ca_base + theme_nothing() + 
+  geom_polygon(data = ca_county, fill = NA, color = "white") +
+  geom_polygon(color = "black", fill = NA)
