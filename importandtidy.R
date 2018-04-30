517c32612fbeccfa8b50c30adbe1da8a2cf21acf
YOUR FULL NAME <jb5622a@student.american.edu>
2018-04-29 20:04:25 -0400

    4.29 Update
    8:05pm

--- importandtidy.R
+++ importandtidy.R
@@ -26,9 +26,32 @@ enddate <- hazeglife$'End Date'
 camplat <- hazeglife$'CampusLat (N/S)'
 camplon <- hazeglife$'CampLong (E/W)'
 campsize <- hazeglife$'Campus Size'
+                                                                       
 hazeglife %>% 
   mutate('Suspension Length' = difftime(startdate, enddate))
 
+# Summary Statistics
+
+unique(minwage$chain)
+filter(minwage, chain == "wendys")
+filter(minwage, chain == "burgerking") 
+# filter(minwage, chain == "kfc") 
+# filter(minwage, chain == "roys")
+# unique(minwage$location)
+# filter(minwage, location == "PA")
+# # filter(minwage, location == "centralNJ")
+# # filter(minwage, location == "northNJ")
+# # filter(minwage, location == "shoreNJ")
+# # filter(minwage, location == "southNJ")
+
+tb2_cases <- filter(table2, type == "cases")$'count'
+tb2_country <- filter(table2, type == "cases")$'country'
+tb2_year <- filter(table2, type == "cases")$'year'
+tb2_population <- filter(table2, type == "population")$'count'
+table2_clean <- tibble(country = tb2_country,
+                       year = tb2_year,
+                       rate = tb2_cases / tb2_population)
+table2_clean
 # Graphing
 
 orgsyr <- tribble(
@@ -109,3 +132,9 @@ ca_base + theme_nothing()
   ca_base + theme_nothing() + 
   geom_polygon(data = ca_county, fill = NA, color = "white") +
   geom_polygon(color = "black", fill = NA)
+  
+# Ohio
+  
+# Pennsylvania 
+  
+# Florida
