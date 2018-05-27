034917bb14566849ccb072117dac30dae427d70c
YOUR FULL NAME <jb5622a@student.american.edu>
2018-05-27 11:17:57 -0400

    5.27 Update

--- importandtidy_updats.R
+++ importandtidy_updats.R
@@ -6,6 +6,7 @@ library(lubridate)
 library(maps)
 library(ggmap)
 library(mapdata)
+library(stringr)
 
 # Import and Tidy
 
@@ -119,5 +120,88 @@ labs <- data.frame(
 greekstates + 
   geom_point(data = labs, aes(x = long, y = lat), color = "red", size = 2)
 
-# Also, wasn't able to figure out the chropleth map due to getting dates and duration
-# of suspension. Any recommendations on how I should best go about it with my data set?
+#breaking down into regions
+
+northeast <- subset(states, region %in% c("california", "oregon", "washington"))
+midatlantic <- subset(states, region %in% c("california", "oregon", "washington"))
+south <- subset(states, region %in% c("california", "oregon", "washington"))
+midwest <- subset(states, region %in% c("california", "oregon", "washington"))
+west <- subset(states, region %in% c("california", "oregon", "washington"))
+northwest <- subset(states, region %in% c("california", "oregon", "washington"))
+
+Getting the california data is easy:
+  
+ca_df <- subset(states, region == "california")
+head(ca_df)
+
+Now, let’s also get the county lines there
+
+counties <- map_data("county")
+ca_county <- subset(counties, region == "california")
+
+head(ca_county)
+
+#Plot the state first but let’s ditch the axes gridlines, and gray background by using the super-wonderful theme_nothing().
+
+ca_base <- ggplot(data = ca_df, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "gray")
+ca_base + theme_nothing()
+
+# Now plot the county boundaries in white:
+  
+ca_base + theme_nothing() + 
+  geom_polygon(data = ca_county, fill = NA, color = "white") +
+  geom_polygon(color = "black", fill = NA)  # get the state border back on top
+
+#Get some facts about the counties
+
+# make a data frame
+x <- readLines("data/ca-counties-wikipedia.txt")
+pop_and_area <- str_match(x, "^([a-zA-Z ]+)County\t.*\t([0-9,]{2,10})\t([0-9,]{2,10}) sq mi$")[, -1] %>%
+  na.omit() %>%
+  str_replace_all(",", "") %>% 
+  str_trim() %>%
+  tolower() %>%
+  as.data.frame(stringsAsFactors = FALSE)
+
+# give names and make population and area numeric
+names(pop_and_area) <- c("subregion", "population", "area")
+pop_and_area$population <- as.numeric(pop_and_area$population)
+pop_and_area$area <- as.numeric(pop_and_area$area)
+
+head(pop_and_area)
+
+We now have the numbers that we want, but we need to attach those to every point on polygons of the counties. This is a job for inner_join from the dplyr package
+
+cacopa <- inner_join(ca_county, pop_and_area, by = "subregion")
+
+And finally, add a column of people_per_mile:
+  
+  cacopa$people_per_mile <- cacopa$population / cacopa$area
+
+head(cacopa)
+
+Now plot population density by county
+
+# prepare to drop the axes and ticks but leave the guides and legends
+# We can't just throw down a theme_nothing()!
+  ditch_the_axes <- theme(
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank()
+  )
+
+elbow_room1 <- ca_base + 
+  geom_polygon(data = cacopa, aes(fill = people_per_mile), color = "white") +
+  geom_polygon(color = "black", fill = NA) +
+  theme_bw() +
+  ditch_the_axes
+elbow_room1 
+
+# This is a job for a scale transformation. Let’s take the log-base-10 of the population density.
+
+elbow_room1 + scale_fill_gradient(trans = "log10")
